f0abcf4c0681ead974d8f4caca0e38ac070110ab
bozicboris <borisbozic35@gmail.com>
2024-06-25 20:38:50 +0200

    initial upload

---
+++ Assets/js/home.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Set a timeout to show the div after 5 seconds (5000 milliseconds)
+    setTimeout(function() {
+        document.getElementById('projects').style.opacity = '1';
+        document.getElementById('contact').style.opacity = '1';
+    }, 4500);
+});
